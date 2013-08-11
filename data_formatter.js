5cba8d072722287ae5a7273441b71cfaf36f28df
A. Samuel Pottinger <Samnsparky@gmail.com>
2013-08-10 19:24:50 -0600

    Conform variable naming to Google standards.
    Conforms data formatting logic to Google's JavaScript style
    guidelines. Possible fix to issue #6.

--- data_formatter.js
+++ data_formatter.js
@@ -38,16 +38,16 @@ function formatAsCSV(corpus, fields, ununsedLabel)
   return Q.fcall(function() {
     var i = 0,
         j = 0,
-        corpus_len = 0,
-        fields_len = 0,
+        corpusLen = 0,
+        fieldsLen = 0,
         rows = [],
         columns = [],
         csv_str = "";
     // Set up header.
     rows.push(fields);
-    for(i = 0, corpus_len = corpus.length; i < corpus_len; ++i) {
+    for(i = 0, corpusLen = corpus.length; i < corpusLen; ++i) {
       columns = [];
-      for(j = 0, fields_len = fields.length; j < fields_len; ++j) {
+      for(j = 0, fieldsLen = fields.length; j < fieldsLen; ++j) {
         if(typeof(corpus[i][fields[j]]) == "number") {
           columns.push(corpus[i][fields[j]]);
         } else {
@@ -78,14 +78,14 @@ function formatAsJSON(corpus, fields, label)
   return Q.fcall(function() {
     var i = 0,
         j = 0,
-        corpus_len = 0,
-        fields_len = 0,
+        corpusLen = 0,
+        fieldsLen = 0,
         item = {},
         json_obj = {};
     json_obj[label] = [];
-    for(i = 0, corpus_len = corpus.length; i < corpus_len; ++i) {
+    for(i = 0, corpusLen = corpus.length; i < corpusLen; ++i) {
       item = {};
-      for(j = 0, fields_len = fields.length; j < fields_len; ++j) {
+      for(j = 0, fieldsLen = fields.length; j < fieldsLen; ++j) {
         item[fields[j]] = corpus[i][fields[j]];
       }
       json_obj[label].push(item);
