d8e12e55dd87e2d1ef6e6967a686c139e5328af6
Erik Cunningham <djtrinary@gmail.com>
2013-08-15 17:07:31 -0600

    config.json for server (subject to change) stub controller test

---
+++ tests/frontend_controller_test.js
@@ -0,0 +1,15 @@
+var test_util = require('./test_util');
+
+
+module.exports = {
+
+    /**
+     * Ensure that configuration settings are being cached in memory.
+     *
+     * @param {nodeunit.test} test Standard nodeunit test object describing the
+     *      test currently running.
+    **/
+    testIndex: function (test) {
+        test.done();
+    }
+};
