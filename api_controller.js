87771301cfa15249c3da37d817d69364d5b12df7
Erik Cunningham <djtrinary@gmail.com>
2013-08-17 16:51:39 -0600

    501 responses for API requests + tests.

--- api_controller.js
+++ api_controller.js
@@ -7,6 +7,33 @@
 
 module.exports = function(app) {
     app.get('/v1', function(req, res) {
-        res.status(200).json({message: "Hello world."});
+      res.status(501).json({message: "API request unimplemented."});
+    });
+
+    app.get('/v1/contributions.json', function(req, res) {
+      res.status(501).json({message: "API request unimplemented."});
+    });
+    app.get('/v1/contributions.csv', function(req, res) {
+      res.status(501)
+        .header("content-type", "text/csv")
+        .send("Message\n501: API request unimplemented.");
+    });
+
+    app.get('/v1/loans.json', function(req, res) {
+      res.status(501).json({message: "API request unimplemented."});
+    });
+    app.get('/v1/loans.csv', function(req, res) {
+      res.status(501)
+        .header("content-type", "text/csv")
+        .send("Message\n501: API request unimplemented.");
+    });
+
+    app.get('/v1/expenditures.json', function(req, res) {
+      res.status(501).json({message: "API request unimplemented."});
+    });
+    app.get('/v1/expenditures.csv', function(req, res) {
+      res.status(501)
+        .header("content-type", "text/csv")
+        .send("Message\n501: API request unimplemented.");
     });
 }
--- tests/api_controller_test.js
+++ tests/api_controller_test.js
@@ -2,18 +2,64 @@ var test_util = require('./test_util');
 var server = require('nodeunit-httpclient').create({
     port: 3030,
     path: '/v1',   //Base URL for requests
-    status: 200,    //Test each response is OK (can override later)
-    headers: {      //Test that each response must have these headers (can override later)
-        'content-type': 'application/json'
-    }
 });
 
 //Automatic tests on response object
 module.exports = {
-    testIndex: function(test) {
-        server.get(test, '/', function(res) {
-          test.deepEqual(res.data, {message: "Hello world."});
+    testIndex: function (test) {
+      server.get(test, '/', function (res) {
+          test.equal(res.statusCode, 501);
+          test.equal(res.headers['content-type'], "application/json");
+          test.deepEqual(res.data, {message: "API request unimplemented."});
           test.done();
         });
-    }
+    },
+    testContributionsJson: function (test) {
+      server.get(test, '/contributions.json', function (res) {
+          test.equal(res.statusCode, 501);
+          test.equal(res.headers['content-type'], "application/json");
+          test.deepEqual(res.data, {message: "API request unimplemented."});
+          test.done()
+      });
+    },
+    testContributionsCsv: function (test) {
+      server.get(test, '/contributions.csv', function (res) {
+          test.equal(res.statusCode, 501);
+          test.equal(res.headers['content-type'], "text/csv");
+          test.deepEqual(res.body, "Message\n501: API request unimplemented.");
+          test.done()
+      });
+    },
+    testLoansJson: function (test) {
+      server.get(test, '/loans.json', function (res) {
+          test.equal(res.statusCode, 501);
+          test.equal(res.headers['content-type'], "application/json");
+          test.deepEqual(res.data, {message: "API request unimplemented."});
+          test.done()
+      });
+    },
+    testLoansCsv: function (test) {
+      server.get(test, '/loans.csv', function (res) {
+          test.equal(res.statusCode, 501);
+          test.equal(res.headers['content-type'], "text/csv");
+          test.deepEqual(res.body, "Message\n501: API request unimplemented.");
+          test.done()
+      });
+    },
+    testExpendituresJson: function (test) {
+      server.get(test, '/expenditures.json', function (res) {
+          test.equal(res.statusCode, 501);
+          test.equal(res.headers['content-type'], "application/json");
+          test.deepEqual(res.data, {message: "API request unimplemented."});
+          test.done()
+      });
+    },
+    testExpendituresCsv: function (test) {
+      server.get(test, '/expenditures.csv', function (res) {
+          test.equal(res.statusCode, 501);
+          test.equal(res.headers['content-type'], "text/csv");
+          test.deepEqual(res.body, "Message\n501: API request unimplemented.");
+          test.done()
+      });
+    },
 }
